51abc15939f2b83ac60eee54d0a5935408daec85
Angel <angelmolinaimas@gmail.com>
2025-07-22 15:55:43 -0400

    adding php task

---
+++ PHP-tasks/programing_logic/exercise_3.php
@@ -0,0 +1,17 @@
+<?php
+$numero1 = 15;
+$numero2 = 4;
+
+$suma = $numero1 + $numero2;
+$resta = $numero1 - $numero2;
+$multiplicacion = $numero1 * $numero2;
+$division = $numero1 / $numero2;
+$residuo = $numero1 % $numero2;
+echo "Número 1: " . $numero1 . "<br>";
+echo "Número 2: " . $numero2 . "<br>";
+echo "Suma: " . $suma . "<br>";
+echo "Resta: " . $resta . "<br>";
+echo "Multiplicación: " . $multiplicacion . "<br>";
+echo "División: " . $division . "<br>";
+echo "Residuo: " . $residuo;
+?>
---
+++ PHP-tasks/programing_logic/exercise_4.php
@@ -0,0 +1,9 @@
+<?php
+$numero = 17;
+
+if ($numero % 2 == 0) {
+    echo "El número " . $numero . " es par";
+} else {
+    echo "El número " . $numero . " es impar";
+}
+?>
